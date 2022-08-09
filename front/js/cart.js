591d5ab7a09f297618b687f90f95e851f5888b38
Thongkham Jean <juanitoo77550@hotmail.fr>
2022-08-09 20:06:51 +0200

    Changement Quantité added

---
+++ front/js/cart.js
@@ -0,0 +1,135 @@
+draw_articles();
+
+function draw_articles() {
+
+    const panier = JSON.parse(localStorage.getItem("panier"));
+    const panierProduit = document.getElementById("cart__items");
+    panierProduit.innerHTML = "";
+
+    console.log(panier);
+
+    for (let article of panier) {
+
+        fetch(`http://localhost:3000/api/products/${article.id}`)
+
+            .then(function (res) {
+                if (res.ok) {
+                    return res.json();
+                }
+            })
+
+            .then(function (value) {
+
+                //Initialisation produit
+                const panierProduitArticle = document.createElement("article");
+                panierProduitArticle.classList.add("cart__item");
+
+                // Image
+                const panierProduitDivImg = document.createElement("div");
+                panierProduitDivImg.classList.add("cart__item__img");
+                const panierProduitImg = document.createElement("img");
+                panierProduitImg.src = value.imageUrl;
+                panierProduitImg.alt = value.altTxt;
+
+                // Contenu du produit
+                const panierProduitContenu = document.createElement("div");
+                panierProduitContenu.classList.add("cart__item__content");
+
+                const panierProduitContenuDescription = document.createElement("div");
+                panierProduitContenuDescription.classList.add("cart__item__content__description");
+
+                const panierProduitTitre = document.createElement("h2");
+                panierProduitTitre.innerText = value.name;
+
+                const panierProduitCouleur = document.createElement("p");
+                panierProduitCouleur.innerText = "Couleur: " + article.color;
+
+                const panierProduitPrix = document.createElement("p");
+                panierProduitPrix.innerText = "Prix: " + value.price * article.quantity + "€";
+
+                //Réglages du produit
+                const panierProduitReglagesDiv = document.createElement("div");
+                panierProduitReglagesDiv.classList.add("cart__item__content__settings");
+
+                const panierProduitReglages = document.createElement("div");
+                panierProduitReglages.classList.add("cart__item__content__settings__quantity");
+
+                // Quantité
+                const panierProduitReglagesQuantite = document.createElement("p");
+                panierProduitReglagesQuantite.innerText = "Quantité: ";
+                const panierProduitReglagesQuantiteChange = document.createElement("input");
+                panierProduitReglagesQuantiteChange.type = "number";
+                panierProduitReglagesQuantiteChange.classList.add("itemQuantity");
+                panierProduitReglagesQuantiteChange.name = "itemQuantity";
+                panierProduitReglagesQuantiteChange.min = "1";
+                panierProduitReglagesQuantiteChange.max = "100";
+                panierProduitReglagesQuantiteChange.value = article.quantity;
+
+                // Suppression Canapé
+                const panierProduitReglagesSupprimerDiv = document.createElement("div");
+                panierProduitReglagesSupprimerDiv.classList.add("cart__item__content__settings__delete");
+                const panierProduitReglagesSupprimer = document.createElement("p");
+                panierProduitReglagesSupprimer.classList.add("deleteItem");
+                panierProduitReglagesSupprimer.innerText = "Supprimer";
+
+                // AppendChilds
+
+                // Image = Enfant de ProduitArticle
+                panierProduitDivImg.appendChild(panierProduitImg);
+                panierProduitArticle.appendChild(panierProduitDivImg);
+
+                // Intérieur Description = Enfant de Description = Enfant de ProduitArticle
+                panierProduitContenuDescription.appendChild(panierProduitTitre);
+                panierProduitContenuDescription.appendChild(panierProduitCouleur);
+                panierProduitContenuDescription.appendChild(panierProduitPrix);
+                panierProduitContenu.appendChild(panierProduitContenuDescription);
+                panierProduitArticle.appendChild(panierProduitContenu);
+
+                // Réglages Quantité = Enfant de Description
+                panierProduitReglages.appendChild(panierProduitReglagesQuantite);
+                panierProduitReglages.appendChild(panierProduitReglagesQuantiteChange);
+                panierProduitReglagesDiv.appendChild(panierProduitReglages)
+                panierProduitContenu.appendChild(panierProduitReglagesDiv);
+
+                // Réglages Suppression = Enfant de Description
+                panierProduitReglagesSupprimerDiv.appendChild(panierProduitReglagesSupprimer)
+                panierProduitContenu.appendChild(panierProduitReglagesSupprimerDiv);
+
+                // Le Parent
+                panierProduit.appendChild(panierProduitArticle);
+
+                //EventListener - Suppression
+                panierProduitReglagesSupprimer.addEventListener("click", supprimerArticle);
+
+                //Fin des AppendChilds
+
+                //Changement Quantité
+                const articleQuantity = document.getElementsByClassName("itemQuantity");
+
+                for (let i = 0; i < articleQuantity.length; i++) {
+                    articleQuantity[i].addEventListener("change", function () {
+                        panier[i].quantity = articleQuantity[i].value;
+                        localStorage.setItem("panier", JSON.stringify(panier));
+                        draw_articles();
+                    })
+                }
+
+                //Calcul Prix Total
+                let prixTotal = 0;
+                prixTotal += 
+                document.getElementById("totalPrice").innerHTML = prixTotal;
+            })
+    }
+}
+
+function supprimerArticle(event) {
+
+    const panier = JSON.parse(localStorage.getItem("panier"));
+    var child = event.target.parentNode.parentNode.parentNode;
+    var parent = child.parentNode;
+    var index = Array.prototype.indexOf.call(parent.children, child);
+    panier.splice(index, 1);
+    window.localStorage.setItem("panier", JSON.stringify(panier));
+
+    draw_articles();
+}
--- front/js/product.js
+++ front/js/product.js
@@ -20,26 +20,26 @@ fetch(`http://localhost:3000/api/products/${id}`) // On récupère l'ID du produ
     image.alt = value.altTxt;
 
     const name = document.getElementById("title");
-    name.innerHTML = value.name;
+    name.innerText = value.name;
 
     const price = document.getElementById("price");
-    price.innerHTML = value.price;
+    price.innerText = value.price;
 
     const description = document.getElementById("description");
-    description.innerHTML = value.description;
+    description.innerText = value.description;
 
     const colors = document.getElementById("colors"); // Je importe "colors" depuis le HTML
 
     value.colors.forEach(function(color) { // Je sélectionne le tableau "colors" dans l'objet "value" et pour chaque color :
         const colors_element = document.createElement("option"); //Je crée une variable "colors_element" qui aura comme balise "option"
         colors_element.value = color; // Dans "colors_element" on rajoute la "value" qui aura comme valeur "color" que l'on importe depuis l'API.
-        colors_element.innerHTML = color; // Je rajoute ce qui s'affiche dans entre les balises "option" qui aura la valeur de "color"
+        colors_element.innerText = color; // Je rajoute ce qui s'affiche dans entre les balises "option" qui aura la valeur de "color"
         colors.appendChild(colors_element); // Je rajoute tout ce que j'ai fait dans la boucle en tant qu'enfant du parent "colors".
     })
 })
 
 const addToCart = document.getElementById("addToCart");
-addToCart.addEventListener("click", addProductToCart);
+addToCart.addEventListener("click", addProductToCart); //Si je clique sur "Ajouter au Panier", éxecute la fonction "addProductToCart"
 var colors = document.getElementById("colors");
 
 function redirectionToCart() {
@@ -49,34 +49,54 @@ function redirectionToCart() {
         )
     ) {
         window.location.href = "cart.html";
+    } else {
+        window.location.href = "index.html";
     }
 }
 
 function addProductToCart() {
 
-    if (!window.localStorage.getItem("panier"))
-        window.localStorage.setItem("panier", JSON.stringify([]));
+    if (!window.localStorage.getItem("panier")) //Si "panier" n'existe pas dans le localStorage
+        window.localStorage.setItem("panier", JSON.stringify([])); //On rajoute la clé "panier" dans le localStorage, dans lequel on crée un tableau en "string" vide dans cette même clé
 
-    let article = {
+    let article = { //On crée l'objet "article"
         id: id,
-        quantity: document.getElementById("quantity").value,
+        quantity: parseInt (document.getElementById("quantity").value),
         color: colors.options[colors.selectedIndex].value
     };
 
-    if (
+    if ( //Si l'article n'est pas vide, est au dessus de 0 et en dessous de 100, on exécute la fonction
         article.color != "" &&
         article.quantity > 0 &&
         article.quantity < 100
     ) {
-        const panier = JSON.parse(window.localStorage.getItem("panier"));
-        panier.push(article);
-        window.localStorage.setItem("panier", JSON.stringify(panier));
+        const panier = JSON.parse(window.localStorage.getItem("panier")); //On crée la constante "panier" qui va prendre la **clé** "panier" depuis le localStorage et le transformer en Objet
+        let listeFinalProduit = [];
+        let dejaPresent = false;
+
+        panier.forEach (function (articleExistant) {
+
+            if (article.id == articleExistant.id && article.color == articleExistant.color) {
+                articleExistant.quantity = article.quantity + parseInt(articleExistant.quantity);
+                listeFinalProduit.push(articleExistant);
+                dejaPresent = true;
+
+            } else {
+                listeFinalProduit.push(articleExistant);
+            }
+        })
+
+        if (!dejaPresent) {
+            listeFinalProduit.push(article);
+        }
+
+        window.localStorage.setItem("panier", JSON.stringify(listeFinalProduit)); //On stocke "panier" dans le localStorage qu'on retransforme son contenu en "string"
 
         document.getElementById("addToCart").textContent = "Produit ajouté !";
         document.getElementById("addToCart").style.color = "rgb(0, 105, 0";
         redirectionToCart();
 
     } else {
-        alert("Veuillez choisir une couleur et/ou une quantité valide")
+        alert("Veuillez choisir une couleur et/ou une quantité valide") //Erreur si les conditions de "if" ne sont pas remplies
     }
 }
