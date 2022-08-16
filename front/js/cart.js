0d945c7832c4860444e981ff346801c9f765d63e
Thongkham Jean <juanitoo77550@hotmail.fr>
2022-08-16 19:05:34 +0200

    Added totalPrice

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,135 +1,152 @@
 draw_articles();
+majPrixQty();
 
 function draw_articles() {
 
-    const panier = JSON.parse(localStorage.getItem("panier"));
-    const panierProduit = document.getElementById("cart__items");
-    panierProduit.innerHTML = "";
+  const panier = JSON.parse(localStorage.getItem("panier"));
+  const panierProduit = document.getElementById("cart__items");
+  panierProduit.innerHTML = "";
 
-    console.log(panier);
+  console.log(panier);
 
-    for (let article of panier) {
+  for (let article of panier) {
 
-        fetch(`http://localhost:3000/api/products/${article.id}`)
+    fetch(`http://localhost:3000/api/products/${article.id}`)
 
-            .then(function (res) {
-                if (res.ok) {
-                    return res.json();
-                }
-            })
+      .then(function (res) {
+        if (res.ok) {
+          return res.json();
+        }
+      })
 
-            .then(function (value) {
+      .then(function (value) {
 
-                //Initialisation produit
-                const panierProduitArticle = document.createElement("article");
-                panierProduitArticle.classList.add("cart__item");
+        //Initialisation produit
+        const panierProduitArticle = document.createElement("article");
+        panierProduitArticle.classList.add("cart__item");
 
-                // Image
-                const panierProduitDivImg = document.createElement("div");
-                panierProduitDivImg.classList.add("cart__item__img");
-                const panierProduitImg = document.createElement("img");
-                panierProduitImg.src = value.imageUrl;
-                panierProduitImg.alt = value.altTxt;
+        // Image
+        const panierProduitDivImg = document.createElement("div");
+        panierProduitDivImg.classList.add("cart__item__img");
+        const panierProduitImg = document.createElement("img");
+        panierProduitImg.src = value.imageUrl;
+        panierProduitImg.alt = value.altTxt;
 
-                // Contenu du produit
-                const panierProduitContenu = document.createElement("div");
-                panierProduitContenu.classList.add("cart__item__content");
+        // Contenu du produit
+        const panierProduitContenu = document.createElement("div");
+        panierProduitContenu.classList.add("cart__item__content");
 
-                const panierProduitContenuDescription = document.createElement("div");
-                panierProduitContenuDescription.classList.add("cart__item__content__description");
+        const panierProduitContenuDescription = document.createElement("div");
+        panierProduitContenuDescription.classList.add("cart__item__content__description");
 
-                const panierProduitTitre = document.createElement("h2");
-                panierProduitTitre.innerText = value.name;
+        const panierProduitTitre = document.createElement("h2");
+        panierProduitTitre.innerText = value.name;
 
-                const panierProduitCouleur = document.createElement("p");
-                panierProduitCouleur.innerText = "Couleur: " + article.color;
+        const panierProduitCouleur = document.createElement("p");
+        panierProduitCouleur.innerText = "Couleur: " + article.color;
 
-                const panierProduitPrix = document.createElement("p");
-                panierProduitPrix.innerText = "Prix: " + value.price * article.quantity + "€";
+        const panierProduitPrix = document.createElement("p");
+        panierProduitPrix.setAttribute("id", "cart__item__price");
+        panierProduitPrix.innerText = "Prix: " + value.price * article.quantity + "€";
 
-                //Réglages du produit
-                const panierProduitReglagesDiv = document.createElement("div");
-                panierProduitReglagesDiv.classList.add("cart__item__content__settings");
+        //Réglages du produit
+        const panierProduitReglagesDiv = document.createElement("div");
+        panierProduitReglagesDiv.classList.add("cart__item__content__settings");
 
-                const panierProduitReglages = document.createElement("div");
-                panierProduitReglages.classList.add("cart__item__content__settings__quantity");
+        const panierProduitReglages = document.createElement("div");
+        panierProduitReglages.classList.add("cart__item__content__settings__quantity");
 
-                // Quantité
-                const panierProduitReglagesQuantite = document.createElement("p");
-                panierProduitReglagesQuantite.innerText = "Quantité: ";
-                const panierProduitReglagesQuantiteChange = document.createElement("input");
-                panierProduitReglagesQuantiteChange.type = "number";
-                panierProduitReglagesQuantiteChange.classList.add("itemQuantity");
-                panierProduitReglagesQuantiteChange.name = "itemQuantity";
-                panierProduitReglagesQuantiteChange.min = "1";
-                panierProduitReglagesQuantiteChange.max = "100";
-                panierProduitReglagesQuantiteChange.value = article.quantity;
+        // Quantité
+        const panierProduitReglagesQuantite = document.createElement("p");
+        panierProduitReglagesQuantite.innerText = "Quantité: ";
+        const panierProduitReglagesQuantiteChange = document.createElement("input");
+        panierProduitReglagesQuantiteChange.type = "number";
+        panierProduitReglagesQuantiteChange.classList.add("itemQuantity");
+        panierProduitReglagesQuantiteChange.name = "itemQuantity";
+        panierProduitReglagesQuantiteChange.min = "1";
+        panierProduitReglagesQuantiteChange.max = "100";
+        panierProduitReglagesQuantiteChange.value = article.quantity;
 
-                // Suppression Canapé
-                const panierProduitReglagesSupprimerDiv = document.createElement("div");
-                panierProduitReglagesSupprimerDiv.classList.add("cart__item__content__settings__delete");
-                const panierProduitReglagesSupprimer = document.createElement("p");
-                panierProduitReglagesSupprimer.classList.add("deleteItem");
-                panierProduitReglagesSupprimer.innerText = "Supprimer";
+        // Suppression Canapé
+        const panierProduitReglagesSupprimerDiv = document.createElement("div");
+        panierProduitReglagesSupprimerDiv.classList.add("cart__item__content__settings__delete");
+        const panierProduitReglagesSupprimer = document.createElement("p");
+        panierProduitReglagesSupprimer.classList.add("deleteItem");
+        panierProduitReglagesSupprimer.innerText = "Supprimer";
 
-                // AppendChilds
+        // AppendChilds
 
-                // Image = Enfant de ProduitArticle
-                panierProduitDivImg.appendChild(panierProduitImg);
-                panierProduitArticle.appendChild(panierProduitDivImg);
+        // Image = Enfant de ProduitArticle
+        panierProduitDivImg.appendChild(panierProduitImg);
+        panierProduitArticle.appendChild(panierProduitDivImg);
 
-                // Intérieur Description = Enfant de Description = Enfant de ProduitArticle
-                panierProduitContenuDescription.appendChild(panierProduitTitre);
-                panierProduitContenuDescription.appendChild(panierProduitCouleur);
-                panierProduitContenuDescription.appendChild(panierProduitPrix);
-                panierProduitContenu.appendChild(panierProduitContenuDescription);
-                panierProduitArticle.appendChild(panierProduitContenu);
+        // Intérieur Description = Enfant de Description = Enfant de ProduitArticle
+        panierProduitContenuDescription.appendChild(panierProduitTitre);
+        panierProduitContenuDescription.appendChild(panierProduitCouleur);
+        panierProduitContenuDescription.appendChild(panierProduitPrix);
+        panierProduitContenu.appendChild(panierProduitContenuDescription);
+        panierProduitArticle.appendChild(panierProduitContenu);
 
-                // Réglages Quantité = Enfant de Description
-                panierProduitReglages.appendChild(panierProduitReglagesQuantite);
-                panierProduitReglages.appendChild(panierProduitReglagesQuantiteChange);
-                panierProduitReglagesDiv.appendChild(panierProduitReglages)
-                panierProduitContenu.appendChild(panierProduitReglagesDiv);
+        // Réglages Quantité = Enfant de Description
+        panierProduitReglages.appendChild(panierProduitReglagesQuantite);
+        panierProduitReglages.appendChild(panierProduitReglagesQuantiteChange);
+        panierProduitReglagesDiv.appendChild(panierProduitReglages)
+        panierProduitContenu.appendChild(panierProduitReglagesDiv);
 
-                // Réglages Suppression = Enfant de Description
-                panierProduitReglagesSupprimerDiv.appendChild(panierProduitReglagesSupprimer)
-                panierProduitContenu.appendChild(panierProduitReglagesSupprimerDiv);
+        // Réglages Suppression = Enfant de Description
+        panierProduitReglagesSupprimerDiv.appendChild(panierProduitReglagesSupprimer)
+        panierProduitContenu.appendChild(panierProduitReglagesSupprimerDiv);
 
-                // Le Parent
-                panierProduit.appendChild(panierProduitArticle);
+        // Le Parent
+        panierProduit.appendChild(panierProduitArticle);
 
-                //EventListener - Suppression
-                panierProduitReglagesSupprimer.addEventListener("click", supprimerArticle);
+        //EventListener - Suppression
+        panierProduitReglagesSupprimer.addEventListener("click", supprimerArticle);
 
-                //Fin des AppendChilds
+        //Fin des AppendChilds
 
-                //Changement Quantité
-                const articleQuantity = document.getElementsByClassName("itemQuantity");
+        //Changement Quantité
+        const articleQuantity = document.getElementsByClassName("itemQuantity");
 
-                for (let i = 0; i < articleQuantity.length; i++) {
-                    articleQuantity[i].addEventListener("change", function () {
-                        panier[i].quantity = articleQuantity[i].value;
-                        localStorage.setItem("panier", JSON.stringify(panier));
-                        draw_articles();
-                    })
-                }
+        for (let i = 0; i < articleQuantity.length; i++) {
+          articleQuantity[i].addEventListener("change", function (event) {
+            panier[i].quantity = articleQuantity[i].value;
+            localStorage.setItem("panier", JSON.stringify(panier));
+            draw_articles();
+            majPrixQty();
 
-                //Calcul Prix Total
-                let prixTotal = 0;
-                prixTotal += 
-                document.getElementById("totalPrice").innerHTML = prixTotal;
-            })
-    }
+            console.log(articleQuantity[i]);
+          })
+        }
+      })
+  }
 }
 
 function supprimerArticle(event) {
 
-    const panier = JSON.parse(localStorage.getItem("panier"));
-    var child = event.target.parentNode.parentNode.parentNode;
-    var parent = child.parentNode;
-    var index = Array.prototype.indexOf.call(parent.children, child);
-    panier.splice(index, 1);
-    window.localStorage.setItem("panier", JSON.stringify(panier));
+  const panier = JSON.parse(localStorage.getItem("panier"));
+  var child = event.target.parentNode.parentNode.parentNode;
+  var parent = child.parentNode;
+  var index = Array.prototype.indexOf.call(parent.children, child);
+  panier.splice(index, 1);
+  window.localStorage.setItem("panier", JSON.stringify(panier));
 
-    draw_articles();
+  draw_articles();
+}
+
+async function majPrixQty(){
+  const panier = JSON.parse(localStorage.getItem("panier"));
+  let sum = 0;
+
+  for(let article of panier) {
+
+    const res = await fetch(`http://localhost:3000/api/products/${article.id}`)
+    const value = await res.json();
+    sum += value.price * article.quantity;
+    console.log(`sum: ${sum} | prix: ${value.price} | quantity: ${article.quantity}`);
+  }
+  
+  const prix_produit = document.getElementById("totalPrice");
+  prix_produit.innerHTML = sum;
+
 }
